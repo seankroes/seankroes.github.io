5027e6012437aa005384a5b1b895aa583ad74d22
seankroes <55536305+seankroes@users.noreply.github.com>
2022-04-17 05:22:00 +0200

    Update DarkMode.js

--- js/DarkMode.js
+++ js/DarkMode.js
@@ -7,36 +7,11 @@ function swapStyleSheet(){
         //pagestyle.setAttribute('href', 'style-theme-dark.css');
         pagestyle.setAttribute('href', 'style-theme-default.css');
         mode = "dark";
-	    
-	    
-	//stop rain
-	    window.removeEventListener("load");
+	   
     } else {
         //pagestyle.setAttribute('href', 'style-theme-default.css');
         pagestyle.setAttribute('href', 'style-theme-dark.css');
-        
-        
-        
-        // Creating rain
-
-window.addEventListener("load", function() {
-	// create rain vars
-	var el = document.createElement("div");
-	el.classList.add("rain")
-	document.getElementById("load").appendChild(el);
-	var frames, duplicate, x, y; // create some vars
-	duplicate = document.getElementById("load").innerHTML;
-	document.getElementById("load").innerHTML = duplicate.repeat(2);
-	frames = document.getElementById("load").children;
-	setInterval(function() {
-		x = document.documentElement.clientWidth - 1, y = document.documentElement.clientHeight - 100; // and y position for the rain drops
-		for(var i = 0; i < frames.length; i++) {
-			frames[i].setAttribute("style", "position:absolute; height:100px; width:1px; left:" + Math.random() * x +"px; top:" + Math.random() * y + "px;")
-		}
-	}, 1);
-
-});
-        
+       
         mode = "default";
     }
 }
